48fb1fc9315a84f6d66381f3628ecbc725f57557
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2022-02-17 11:36:39 -0500

    completed task 4

--- main.cpp
+++ main.cpp
@@ -6,16 +6,21 @@
 
 int main()
 {
-  // std::cout << get_east_storage("01/01/2018") << "billon gallons\n";
-  // // std::cout << get_east_storage("03/01/2018") << "billon gallons\n";
-  // // std::cout  << get_east_storage("02/06/2018") << "billon gallons\n";
-  // std::cout << "minimum storage in East basin:" << get_min_east() << "billon gallons\n";
-  // std::cout << "maximum storage in East basin:" << get_max_east() << "billon gallons\n";
-  // std::cout << "01/01/2018" << compare_basins("01/01/2018") << std::endl;
-  // std::cout << "01/01/2018" << compare_basins("01/06/2018") << std::endl;
-  // std::cout << "\n"
-  std::cout << "Reserve Order from 01/01/2018", "01/03/2018";
-  reverse_order("01/01/2018", "01/03/2018");    
+std::cout << get_east_storage("01/01/2018") << " billon gallons\n";
+std::cout << get_east_storage("03/01/2018") << " billon gallons\n";
+std::cout  << get_east_storage("02/06/2018") << " billon gallons\n";
+std::cout << "\n";
+
+std::cout << "minimum storage in East basin:" << get_min_east() << " billon gallons\n";
+std::cout << "maximum storage in East basin:" << get_max_east() << " billon gallons\n";
+std::cout << "\n";
+
+std::cout << "01/01/2018" << compare_basins("01/01/2018") << std::endl;
+std::cout << "01/01/2018" << compare_basins("01/06/2018") << std::endl;
+std::cout << "\n";
+
+std::cout << "Reserve Order from 05/29/2018 to 06/02/2018";
+reverse_order("05/29/2018", "06/02/2018");  
 
   return 0;
 }
--- reserveorder.cpp
+++ reserveorder.cpp
@@ -7,26 +7,58 @@
 
 void reverse_order(std::string date1, std::string date2)
 {
+    //opens the file
     std::ifstream fin("Current_Reservoir_Levels.tsv");
 
+    // gives this error if file cant be open
     if (fin.fail()) 
     {
         std::cout << "File cannot be opened for reading." << std::endl;
-        exit(1); // exit if failed to open the file
+        exit(1); 
     } 
 
+    //removes the first line that does not have the the information we need
     std::string junk, date;
     getline(fin, junk);
-    array[363];
 
+    double day = 0;
+    bool pass;
+    double elevation[365];
+    int counter = 0;
     double eastSt, eastEl,westEl,westSt;
+    
+    //loops through and labels what each value is represents up until the west elevation
     while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) 
     { 
+    // ignores all the information after west elevation
         fin.ignore(INT_MAX, '\n');
-        
-       
+
+    // given the first date within the function parameter if its within the the file pass the information through to the next step
+        if (date == date1)
+        {
+            pass = true;
+        }
+    // the first date will repersent the index 0 in the array that was made before 
+    // every value starting from the first date is put an index. the counter represents the position from the first value to whatever the last value is
+    // the values of the corresponding west elevation values are then put in with the apporaite index's which represents 
+        if (pass == true)
+        {
+            elevation[counter] = westEl;
+            counter++;
+        }
+    // the second date represents the last date. therfore the last value that is being put
+    // so everything after this date should not be put in the array 
+        if (date == date2)
+        {
+            pass = false;
+
+        }
     }
-fin.close();
-return 0;
+    // this loop prints out the array in desending order 
+    for (int i = counter; i >= 0; i--)
+        {
+        std::cout << elevation[i] << std::endl;
+        }
 }
+
 
